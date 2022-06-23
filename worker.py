5521195b5c6206aeb097f8a75750fe1b1e4a5f85
Dawadi <adawadi6@icl.gtri.org>
2022-06-23 14:22:03 -0400

    Push

--- worker.py
+++ worker.py
@@ -3,6 +3,12 @@ import os
 import redis
 from rq import Worker, Queue, Connection
 
+redis_url = os.getenv('REDISTOGO_URL')
+
+urlparse.uses_netloc.append('redis')
+url = urlparse.urlparse(redis_url)
+conn = Redis(host=url.hostname, port=url.port, db=0, password=url.password)
+
 listen = ['high', 'default', 'low']
 
 redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
